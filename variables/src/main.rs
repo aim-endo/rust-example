65d1bfbd57aab29afa98aa894b49a084d0adf160
Yuma Endo <endo@ai-ms.com>
2022-11-24 18:26:09 +0900

    variables example

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -1,3 +1,18 @@
+
+const MAX_POINTS: u32 = 100_000;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Max value = {}", MAX_POINTS);
+
+    let x = 5;
+    println!("The value of x is: {}", x);
+
+    let x = x + 1;
+
+    {
+        let x = x * 2;
+        println!("The value of x in the inner scope is: {}", x);
+    }
+
+    println!("The value of x is: {}", x);
 }
