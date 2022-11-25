17804af04aaf276ce987d7f00bb66577b9d27aca
Yuma Endo <endo@ai-ms.com>
2022-11-25 18:05:45 +0900

    rectangles: method definition

--- rectangles/src/main.rs
+++ rectangles/src/main.rs
@@ -4,14 +4,16 @@ struct Rectangle {
     height: u32,
 }
 
+impl Rectangle {
+    fn area(&self) -> u32 {
+        self.width * self.height
+    }
+}
+
 fn main() {
     let rect1 = Rectangle { width: 30, height: 50 };
 
-    println!("The area of the rectangle is {} square pixels.", area(&rect1));
+    println!("The area of the rectangle is {} square pixels.", rect1.area());
     println!("rect1 is {:?}", rect1);
     println!("rect1 is {:#?}", rect1);
-}
-
-fn area(rectangle: &Rectangle) -> u32 {
-    rectangle.width * rectangle.height
 }
